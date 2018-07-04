56550052049037c3635624cdbedbebd188d18c20
Kay van Bree <kayvanbree@gmail.com>
2018-07-04 17:02:26 +0200

    Fetch todos and print in console

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {TodoService} from '../../providers/todo.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private todoSub: Subscription;
 
-  ngOnInit() {
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.todoSub = this.todoService.getAll().subscribe((result) => {
+      console.log(result);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.todoSub.unsubscribe();
+  }
 }
