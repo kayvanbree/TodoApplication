df2e0e44103b5ad2ef684e4dbcd59fbc4e66e441
Kay van Bree <kayvanbree@gmail.com>
2018-07-12 09:41:05 +0200

    Add HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import {HttpClientModule} from '@angular/common/http';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
 import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
-    TodoListItemComponent
+    TodoListItemComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
